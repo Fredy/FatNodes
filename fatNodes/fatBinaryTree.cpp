5b02ef2f268feebeea0edaa2fa2fdca86bf7ce5f
Fredy Álvarez <fre.alvarezb@gmail.com>
2017-11-07 22:59:24 -0500

    FatBinaryTree: added some functions

--- fatNodes/fatBinaryTree.cpp
+++ fatNodes/fatBinaryTree.cpp
@@ -1,3 +1,48 @@
 #include "fatBinaryTree.hpp"
+#include "fatNode.hpp"
+#include "types.hpp"
 
-FatBinaryTree::FatBinaryTree() {}
+FatBinaryTree::FatBinaryTree() {
+  this->root = nullptr;
+  this->currentVer = 0;
+}
+
+FatNode **FatBinaryTree::find(int x) {
+  FatNode **ptr = &this->root;
+
+  while (*ptr and (*ptr)->value != x) {
+    if (x > (*ptr)->value)
+      ptr = &((*ptr)->getLeft(this->currentVer));
+    else
+      ptr = &((*ptr)->getRight(this->currentVer));
+  }
+  return ptr;
+}
+
+FatNode *FatBinaryTree::findParent(int x) {
+  FatNode *ptr = &this->root;
+  FatNode *ret;
+  while (ptr and ptr->value != x) {
+    ret = ptr;
+    if (x > ptr->value)
+      ptr = ptr->getLeft(this->currentVer);
+    else
+      ptr = ptr->getRight(this->currentVer);
+  }
+  return ret;
+}
+
+void FatBinaryTree::insert(int x) {
+  FatNode *fnd = this->findParent(x);
+
+  if (x > fnd->value) {
+    if (!fnd->getLeft(this->currentVer))
+      fnd->inject(x, Side::LEFT);
+  } else {
+    if (!fnd->getRight(this->currentVer))
+      fnd->inject(x, Side::RIGHT);
+  }
+
+}
+
+void FatBinaryTree::remove(int x);
--- fatNodes/fatBinaryTree.hpp
+++ fatNodes/fatBinaryTree.hpp
@@ -2,13 +2,31 @@
 #include <vector>
 using namespace std;
 
+class FatNode;
 class FatBinaryTree {
 public:
   size_t currentVer;
+  FatNode *root;
 
   FatBinaryTree();
+  ~FatBinaryTree() = default;
   // To find the current version on nodes:
   // check for versions <= to the current version
   void redo();
   void undo();
+
+  //--------------------
+  // Binary tree methods
+  //--------------------
+
+  // Returns *nullptr (a pointer pointing to a nullptr) if it doesn't find the
+  // element
+  FatNode **find(int x);
+
+  // Returns the previous element if it doesn't find the searched element
+  FatNode *findParent(int x);
+  void insert(int x);
+  void remove(int x);
+
+  // TODO: clear and destructor
 };
